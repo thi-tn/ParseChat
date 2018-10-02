5345c312b8a8c749b699f22b19e8ca6e297ceff3
samuelraymond23 <samuelraymond23@yahoo.com>
2018-10-02 02:20:37 -0400

    2nd commit

--- ParseChat/LoginViewController.swift
+++ ParseChat/LoginViewController.swift
@@ -19,6 +19,25 @@ class LoginViewController: UIViewController {
             if user != nil {
                 print("logged in")
                 self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            } else {
+                let error = error?.localizedDescription
+                let alertController = UIAlertController(title: "Alert", message: "Error: " + error!, preferredStyle: .alert)
+                // create a cancel action
+                let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) { (action) in
+                    // handle cancel response here. Doing nothing will dismiss the view.
+                }
+                // add the cancel action to the alertController
+                alertController.addAction(cancelAction)
+                
+                // create an OK action
+                let OKAction = UIAlertAction(title: "OK", style: .default) { (action) in
+                    // handle response here.
+                }
+                // add the OK action to the alert controller
+                alertController.addAction(OKAction)
+                self.present(alertController, animated: true) {
+                    // optional code for what happens after the alert controller has finished presenting
+                }
             }
         }
     }
@@ -27,20 +46,45 @@ class LoginViewController: UIViewController {
         newUser.username = usernameField.text
         newUser.password = passwordField.text
         newUser.signUpInBackground { (success, error) in
-            if success {
-                print("created a user")
-                self.performSegue(withIdentifier: "loginSegue", sender: nil)            } else {
-                print(error?.localizedDescription)
-                if error?._code == 202 {
-                    print("user name is taken")
+            if (self.usernameField.text?.isEmpty)! || (self.passwordField.text?.isEmpty)! {
+                let alertController = UIAlertController(title: "Alert", message: "Username or password is missing.", preferredStyle: .alert)
+                // create a cancel action
+                let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) { (action) in
+                    // handle cancel response here. Doing nothing will dismiss the view.
+                }
+                // add the cancel action to the alertController
+                alertController.addAction(cancelAction)
+                
+                // create an OK action
+                let OKAction = UIAlertAction(title: "OK", style: .default) { (action) in
+                    // handle response here.
+                }
+                // add the OK action to the alert controller
+                alertController.addAction(OKAction)
+                self.present(alertController, animated: true) {
+                    // optional code for what happens after the alert controller has finished presenting
+                }
+            } else {
+                if success {
+                    print("created a user")
+                    self.performSegue(withIdentifier: "loginSegue", sender: nil)
+                } else {
+                    print(error?.localizedDescription)
+                    
+                    if error?._code == 202 {
+                        print("user name is taken")
+                    }
                 }
             }
+            
+                
         }
     }
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+      
     }
 
     override func didReceiveMemoryWarning() {
